4abdc53a2ba073a0bfbbd05117e178f76aaab619
Sergiy <37virs85@gmail.com>
2023-02-18 16:22:35 +0200

    docs

--- app/Http/Controllers/Blog/Admin/BaseController.php
+++ app/Http/Controllers/Blog/Admin/BaseController.php
@@ -4,6 +4,14 @@ namespace App\Http\Controllers\Blog\Admin;
 
 use App\Http\Controllers\Blog\BaseController as GuestBaseController;
 
+/**
+ * Base controller for each blog controller
+ * in admin panel
+ *
+ * MUST BE parent of each manage blog controllers
+ *
+ * @package App\Http\Contoller\Blog\Admin
+ */
 abstract class BaseController extends GuestBaseController
 {
     public function __construct()
