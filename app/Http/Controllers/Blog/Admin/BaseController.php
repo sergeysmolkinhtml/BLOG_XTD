66809ab5f32596c962f749c4f35d4f933baa5fd7
Sergiy <37virs85@gmail.com>
2023-02-18 14:23:47 +0200

    construct for rightness :)

--- app/Http/Controllers/Blog/Admin/BaseController.php
+++ app/Http/Controllers/Blog/Admin/BaseController.php
@@ -6,5 +6,8 @@ use App\Http\Controllers\Blog\BaseController as GuestBaseController;
 
 abstract class BaseController extends GuestBaseController
 {
+    public function __construct()
+    {
 
+    }
 }
