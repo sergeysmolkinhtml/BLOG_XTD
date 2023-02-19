68274ac58ac3bea48125f02be644cbd6bb676eff
Sergiy <37virs85@gmail.com>
2023-02-19 10:46:06 +0200

    two methods relations

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(BlogCategory::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
 }
