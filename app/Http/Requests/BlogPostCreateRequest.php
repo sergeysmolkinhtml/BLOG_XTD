53fcf1e3a6179fccc0073fb47a8cdda4563ac17e
Sergiy <37virs85@gmail.com>
2023-02-20 20:37:20 +0200

    blog post create request validation

---
+++ app/Http/Requests/BlogPostCreateRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BlogPostCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title'        => 'required|min:5|max:200|unique:blog_posts',
+            'slug'         => 'max:200',
+            'content_raw'  => 'required|string|min:3|max:10000',
+            'category_id'  => 'required|integer|exists:blog_categories,id',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required'  => 'Enter a header please',
+            'content_raw.min' => 'Minimum article length is [:min] symbols'
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'title' => 'Title',
+        ];
+    }
+
+}
--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -30,6 +30,8 @@ class BlogPost extends Model
     use HasFactory;
     use SoftDeletes;
 
+    const UNKNOWN_USER = 1;
+
     protected $fillable =
         [
             'title',
@@ -39,7 +41,7 @@ class BlogPost extends Model
             'content_raw',
             'is_published',
             'published_at',
-            'user_id'
+
         ];
 
 
